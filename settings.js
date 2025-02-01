87cbceed6e4a9f6ffb6a31de56bbf3a4b72d4749
Kennwalker <kennwalker222@gmail.com>
2025-02-01 08:02:33 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,6 +6,7 @@ const settings = {
   ownerNumber: '12136061765', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
+  autostatusview: "true"
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
